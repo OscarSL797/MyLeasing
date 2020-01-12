b3391fbcaaa11e9b8ce23051c103881e676e0223
Oscar SL <oscarsoto040797@gmail.com>
2020-01-11 19:52:44 -0600

    Added the OwnersController and the views

---
+++ MyLeasing.Web/Data/Entities/Owner.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyLeasing.Web.Data.Entities
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+
+        [MaxLength(30, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Document { get; set; }
+
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Display(Name = "Fixed Phone")]
+        public string FixedPhone { get; set; }
+
+        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Display(Name = "Cellphone")]
+        public string CellPhone { get; set; }
+
+        public string Address { get; set; }
+
+        [Display(Name ="Full Name")]
+        public string FullName => $"{FirstName} {LastName}";
+        public string FullNameWithDocument => $"{FirstName} {LastName} - {Document}";
+    }
+}
