281d2970056c659f0f67ca39596b7492880cbef3
Oscar SL <oscarsoto040797@gmail.com>
2020-01-15 09:01:00 -0600

    Complete DB

--- MyLeasing.Web/Data/Entities/Lessee.cs
+++ MyLeasing.Web/Data/Entities/Lessee.cs
@@ -3,39 +3,39 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MyLeasing.Web.Data.Entities
 {
-    public class Owner
+    public class Lessee
     {
         public int Id { get; set; }
 
-        [MaxLength(30, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Display(Name = "Document")]
+        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
         [Required(ErrorMessage = "The field {0} is mandatory.")]
         public string Document { get; set; }
 
+        [Display(Name = "First Name")]
         [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
         [Required(ErrorMessage = "The field {0} is mandatory.")]
-        [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
+        [Display(Name = "Last Name")]
         [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
         [Required(ErrorMessage = "The field {0} is mandatory.")]
-        [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
-        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
         [Display(Name = "Fixed Phone")]
+        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
         public string FixedPhone { get; set; }
 
+        [Display(Name = "Cell Phone")]
         [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
-        [Display(Name = "Cellphone")]
         public string CellPhone { get; set; }
 
+        [MaxLength(100, ErrorMessage = "The {0} field can not have more than {1} characters.")]
         public string Address { get; set; }
-
-        [Display(Name ="Full Name")]
-        public string FullName => $"{FirstName} {LastName}";
-        public string FullNameWithDocument => $"{FirstName} {LastName} - {Document}";
-
-        public ICollection<Property> Properties { get; set; }
+        [Display(Name ="Lessee")]
         public ICollection<Contract> Contracts { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+
+        public string FullNameWithDocument => $"{FirstName} {LastName} - {Document}";
     }
 }
